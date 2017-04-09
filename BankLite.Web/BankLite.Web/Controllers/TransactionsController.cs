45e8ac8298994f6bebbd934f1f3bf66855a65fd7
MKrsic <krsicmatija@gmail.com>
2017-04-09 20:29:48 +0200

    Premjestene stvari za User-a u root.

--- BankLite.Web/BankLite.Web/Controllers/TransactionsController.cs
+++ BankLite.Web/BankLite.Web/Controllers/TransactionsController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace BankLite.Web.Areas.User.Controllers
+namespace BankLite.Web.Controllers
 {
     public class TransactionsController : Controller
     {
