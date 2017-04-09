e5f4579fa100a9e2de7a23b2afac1783449e211c
MKrsic <krsicmatija@gmail.com>
2017-04-09 18:15:54 +0200

    Dodan kontroler za prikaz klijentskih transakcija i partial sa tablicom u kojoj ce se prikazivati popis transakcija.

---
+++ BankLite.Web/BankLite.Web/Areas/User/Controllers/TransactionsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BankLite.Web.Areas.User.Controllers
+{
+    public class TransactionsController : Controller
+    {
+        // GET: User/Transactions
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
